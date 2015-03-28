4a7d6c03a2161f14fb00f0a673465ee5554e894f
Neil Kandalgaonkar <neilk@brevity.org>
2015-03-27 18:22:13 -0700

    logging, do not schedule events in past

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 var argv = require('minimist')(process.argv.slice(2)),
+    bunyan = require('bunyan'),
     spawn = require('child_process').spawn,
     moment = require('moment'),
     schedule = require('node-schedule');
@@ -7,14 +8,16 @@ var argv = require('minimist')(process.argv.slice(2)),
 var APPLESCRIPT = '/usr/bin/osascript';
 var SCRIPT_TEMPLATE = 'display notification "%(text)s" with title "%(title)s" sound name "%(sound)s"';
 
+var Log;
+var StartMoment = moment();
 
 var callbacks = {
   up: function() {
-    console.log("up!");
+    Log.info("up!");
     toast("Desk up!", "Time to stand!", "BeepRising");
   },
   down: function() {
-    console.log("down!");
+    Log.info("down!");
     toast("Desk down!", "Time to sit.", "BeepFalling");
   }
 }
@@ -22,41 +25,52 @@ var callbacks = {
 function toast(title, text, sound) {
   var args = { title: title, text: text, sound: sound };
   var script = sprintf(SCRIPT_TEMPLATE, args);
-  console.log(script);
   spawn('/usr/bin/osascript', ['-e', script]);
 }
 
 function loadConfig(config) {
-  var jobCount = 0;
+  var startDate = StartMoment.toDate();
 
   Object.keys(config).forEach(function(callbackName) {
     var callback = callbacks[callbackName];
     config[callbackName].forEach(function(dateStr) {
       var date = moment(dateStr).toDate();
-      console.log('scheduled', date, callbackName);
-      var job = schedule.scheduleJob(date, callback);
+      if (date >= startDate) {
+        schedule.scheduleJob(date, callback);
+        Log.info({'scheduled': [date, callbackName]});
+      }
     })
-    jobCount++;
   });
 
-  console.log(jobCount.toString() + " jobs scheduled.");
 }
 
 function getConfig(argv) {
   var config = {};
   if ('test' in argv) {
-    var now = moment();
     config = {
-      up:   [ now.clone().add(2, 'seconds').format('YYYY-MM-DD HH:mm:ss') ],
-      down: [ now.clone().add(4, 'seconds').format('YYYY-MM-DD HH:mm:ss') ],
+      up:   [
+                StartMoment.clone().add(2, 'seconds').format('YYYY-MM-DD HH:mm:ss'),
+                StartMoment.clone().subtract(2, 'seconds').format('YYYY-MM-DD HH:mm:ss')
+            ],
+      down: [ StartMoment.clone().add(4, 'seconds').format('YYYY-MM-DD HH:mm:ss') ],
     }
-    console.log(config);
+    Log = bunyan.createLogger({name: 'standschedule'})
+    Log.info(config);
   } else {
     var configFile = './config.json';
     if ('config' in argv) {
       configFile = argv['config'];
     }
     config = require(configFile);
+    Log = bunyan.createLogger({
+      name: 'standschedule',
+      streams: [{
+        type: 'rotating-file',
+        path: '/var/Log/standschedule.Log',
+        period: '1d',   // daily rotation
+        count: 3        // keep 3 back copies
+      }]
+    });
   }
   return config;
 }
